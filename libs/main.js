5bb894b52e70ed21a47a8d3d43e0115cdfbecacd
Tomoya Koyanagi <tomk79@gmail.com>
2015-07-02 12:23:59 +0900

    added: addProject(), getProjectAll(), getProject(), removeProject()

--- libs/main.js
+++ libs/main.js
@@ -11,9 +11,7 @@ module.exports = new (function(){
 		this.pathDataDir = pathDataDir;
 		this.options = options;
 		this.db = {};
-		this.load(function(db){
-			_this.db = db;
-		});
+		this.db = this.loadSync();
 	}
 
 	/**
@@ -28,6 +26,17 @@ module.exports = new (function(){
 		});
 
 		return this;
+	}
+
+	/**
+	 * データを読み込む(同期)
+	 */
+	px2dtLocalDataAccess.prototype.loadSync = function(){
+		var db = {};
+		if( fs.existsSync(this.pathDataDir+'/db.json') ){
+			db = require( this.pathDataDir+'/db.json' );
+		}
+		return db;
 	}
 
 	/**
@@ -58,7 +67,70 @@ module.exports = new (function(){
 	}
 
 	/**
-	 * プロジェクトを作成する
+	 * プロジェクト情報を追加する
+	 */
+	px2dtLocalDataAccess.prototype.addProject = function(pjInfo, cb){
+		cb = cb || function(){};
+		this.db = this.db || {};
+		this.db.projects = this.db.projects || [];
+
+		if(typeof(pjInfo) !== typeof({})){ cb(false);return this; }
+		if(typeof(pjInfo.name) !== typeof('')){ cb(false);return this; }
+		if(typeof(pjInfo.path) !== typeof('')){ cb(false);return this; }
+		if(typeof(pjInfo.entry_script) !== typeof('')){ cb(false);return this; }
+
+		this.db.projects.push(pjInfo);
+
+		this.db.projects.sort(function(a,b){
+			var aStr = a.name.toLowerCase();
+			var bStr = b.name.toLowerCase();
+			if(aStr > bStr)  return 1;
+			if(aStr < bStr)  return -1;
+			return 0;
+		});
+
+		cb(true);
+		return this;
+	}
+
+	/**
+	 * プロジェクト情報の一覧を取得する
+	 */
+	px2dtLocalDataAccess.prototype.getProjectAll = function(cb){
+		cb = cb || function(){};
+		cb(this.db.projects);
+		return this;
+	}
+
+	/**
+	 * プロジェクト情報を取得する
+	 */
+	px2dtLocalDataAccess.prototype.getProject = function(pjCd, cb){
+		cb = cb || function(){};
+		cb(this.db.projects[pjCd]);
+		return this;
+	}
+
+	/**
+	 * プロジェクト情報を削除する
+	 */
+	px2dtLocalDataAccess.prototype.removeProject = function(pjCd, cb){
+		cb = cb || function(){};
+		if(typeof(pjCd) != typeof(0)){
+			cb(false);
+			return this;
+		}
+
+		var beforeLen = this.db.projects.length;
+		this.db.projects.splice( pjCd, 1 );
+		var afterLen = this.db.projects.length;
+
+		cb( beforeLen === (afterLen+1) );
+		return this;
+	}
+
+	/**
+	 * Px2DTLDAオブジェクトを作成する
 	 */
 	this.create = function(pathDataDir, options){
 		var px2dtLDA = new px2dtLocalDataAccess(pathDataDir, options);
--- tests/mainTest.js
+++ tests/mainTest.js
@@ -45,6 +45,81 @@ describe('データディレクトリを初期化するテスト', function() {
 			done();
 		});
 	});
+});
+
+describe('プロジェクト情報の入出力', function() {
+	var px2dtLDA = require('../libs/main.js').create(_baseDir, {});
+
+	it("プロジェクト情報を追加するテスト", function(done) {
+		px2dtLDA.addProject(
+			{
+				"name":"TestProject2",
+				"path":__dirname+'/stub_datadir/htdocs2/',
+				'entry_script':'.px_execute.php'
+			},
+			function(result){
+				assert.ok( result );
+
+				px2dtLDA.addProject(
+					{
+						"name":"TestProject1",
+						"path":__dirname+'/stub_datadir/htdocs1/',
+						'entry_script':'.px_execute.php'
+					},
+					function(result){
+						assert.ok( result );
+						done();
+					}
+				);
+			}
+		);
+	});
+
+	it("プロジェクト情報の一覧を取得するテスト", function(done) {
+		px2dtLDA.getProjectAll(
+			function(result){
+				assert.equal( result[0].name, "TestProject1" );
+				assert.equal( result[0].entry_script, ".px_execute.php" );
+				assert.equal( result[1].name, "TestProject2" );
+				assert.equal( result[1].entry_script, ".px_execute.php" );
+				done();
+			}
+		);
+	});
+
+	it("プロジェクト情報を取得するテスト", function(done) {
+		px2dtLDA.getProject(
+			0,
+			function(result){
+				assert.equal( result.name, "TestProject1" );
+				assert.equal( result.entry_script, ".px_execute.php" );
+				done();
+			}
+		);
+	});
+
+	it("プロジェクト情報を削除するテスト", function(done) {
+		px2dtLDA.removeProject(
+			0,
+			function(result){
+				assert.ok( result );
+
+				px2dtLDA.getProject(
+					0,
+					function(result){
+						assert.equal( result.name, "TestProject2" );
+						assert.equal( result.entry_script, ".px_execute.php" );
+						done();
+					}
+				);
+			}
+		);
+	});
+
+});
+
+describe('テスト後にデータディレクトリを削除する', function() {
+	var px2dtLDA = require('../libs/main.js').create(_baseDir, {});
 
 	it("テスト後の後始末", function(done) {
 		dataClean(function(result){
